60df18ffcfb99a472c3ff168aa1869b19fe78961
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-01-13 10:24:32 +0800

    Create login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,59 @@
-import Image from "next/image";
+const Login = () => {
+    return (
+        <div className="w-screen min-h-screen bg-off-white flex">
+            <div className="w-1/2 h-screen bg-[url('/gradient.png')] bg-cover bg-no-repeat bg-center flex justify-center items-center">
+                <p className="font-inter font-bold tracking-tight text-6xl">
+                    Welcome to <br />
+                    Muhammadiyah
+                    <br />
+                    Welfare Home's
+                    <br /> Minimart!
+                </p>
+            </div>
+            <div className="w-1/2 flex items-center justify-center">
+                <div className="bg-white shadow-custom font-inter flex flex-col w-full m-20 p-12 rounded-2xl gap-y-8 flex items-center justify-center">
+                    <div className="gap-y-4 flex flex-col w-full justify-center items-center">
+                        <img
+                            src="/mwh-logo.png"
+                            alt="Welfare Home Logo"
+                            className="w-52"
+                        />
+                        <p className="text-5xl font-black text-blue text-center">
+                            Welcome Back
+                        </p>
+                    </div>
+                    <div className="w-full flex flex-col gap-y-4 text-sm">
+                        <div className="flex flex-col w-full mb-1">
+                            <label className="text-black font-medium">
+                                Email
+                            </label>
+                            <input
+                                placeholder="Enter your email here."
+                                className="w-full bg-input px-5 py-2.5 rounded-xl text-black"></input>
+                        </div>
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                        <div className="flex flex-col w-full">
+                            <label className="text-black font-medium mb-1">
+                                Password
+                            </label>
+                            <input
+                                placeholder="Enter your password here."
+                                type="password"
+                                className="w-full bg-input px-5 py-2.5 rounded-xl text-black"></input>
+                        </div>
+                    </div>
+                    <button className="bg-red rounded-xl px-5 py-2.5 font-semibold font-white text-sm">
+                        LOGIN
+                    </button>
+                    <p className="text-blue font-medium text-center text-sm">
+                        Please reach out to our admin if you would like to
+                        <br />
+                        create an account or reset your password.
+                    </p>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    );
+};
+
+export default Login;
