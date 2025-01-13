c5371d13f6ab5920a2541f2f207913b936f7fe05
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-01-14 00:01:51 +0800

    Add background color and min height to PageWithNavbar. To use wrap the page's children with this component.

--- app/components/PageWithNavbar.tsx
+++ app/components/PageWithNavbar.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 const PageWithNavbar = ({ children }: { children: ReactNode }) => {
     return (
-        <div>
+        <div className="min-h-screen bg-off-white">
             <Navbar />
             <div className="pt-18">{children}</div>
         </div>
