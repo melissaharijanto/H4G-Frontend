d79c70151ded9c83c4ef6b271d6ba9d892ddbf0c
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-01-15 22:27:14 +0700

    fix: remove test code

--- lib/backend/common.tsx
+++ lib/backend/common.tsx
@@ -3,11 +3,9 @@ import { useAppSelector } from '@/lib/hooks';
 import { jwtDecode } from 'jwt-decode';
 
 export function getJwt(): string{
-    // const session = useAppSelector((state) => state.session);
-    // const jwt = session.jwt;
-    // return jwt;
-    // return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczNjk0NjU2NSwianRpIjoiNTdiYTg3YmEtYzU2ZS00NTA1LWEzY2EtZGZjMjU3N2E5MjExIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InUwMDAxIiwibmJmIjoxNzM2OTQ2NTY1LCJjc3JmIjoiNjYzMjQzMTAtMDczNy00NTNiLTg5YzktNGYwZDhiNmMwNWU4IiwiZXhwIjoxNzM3MDMyOTY1fQ.8hv961815pEjGZK8eCQMkZ32C4WMsA6b8p4y9kwre2E";
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczNjk0NzY1MiwianRpIjoiZDIwYzJmYWUtM2Q4My00NGJjLWI3ZGItODVkNTZjZTBhNDcyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InUwMDAyIiwibmJmIjoxNzM2OTQ3NjUyLCJjc3JmIjoiODEwMzljMzItN2RmMy00Mjc0LWFiMGUtNjNlYWU3ZDY2MTAzIiwiZXhwIjoxNzM3MDM0MDUyfQ.tY73N0D0v8H5EObMvoFP14S2bhUetif6rw-vrWYieGE";
+    const session = useAppSelector((state) => state.session);
+    const jwt = session.jwt;
+    return jwt;
 }
 
 export function getUid(): string | undefined {
