ea07cef08fc5dd9eaccfc9acdc1cee04470d16cb
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-01-16 00:10:04 +0800

    Remove unused functions

--- lib/backend/common.tsx
+++ lib/backend/common.tsx
@@ -1,20 +1,23 @@
-import { API_URL } from "@/app/constants";
-import { useAppSelector } from '@/lib/hooks';
-import { jwtDecode } from 'jwt-decode';
+import { API_URL } from '@/app/constants';
 
-export async function call<T>(path: string, method: string, jwt: string, body?: any): Promise<T> {
+export async function call<T>(
+    path: string,
+    method: string,
+    jwt: string,
+    body?: any
+): Promise<T> {
     if (body) {
-        console.log(JSON.stringify(body))
+        console.log(JSON.stringify(body));
     }
 
     const response = await fetch(`${API_URL}${path}`, {
-      method: method.toUpperCase(), 
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${jwt}`,
-    },
-      body: body ? JSON.stringify(body) : undefined,
+        method: method.toUpperCase(),
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${jwt}`,
+        },
+        body: body ? JSON.stringify(body) : undefined,
     });
-  
+
     return response.json() as Promise<T>;
 }
