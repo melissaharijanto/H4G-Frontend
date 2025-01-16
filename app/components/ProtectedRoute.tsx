1a02798cb914506ac28629130dbc519d61fcbdbe
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-01-16 16:02:56 +0800

    Add protected route component

---
+++ app/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { ReactNode } from 'react';
+import { redirect } from 'next/navigation';
+import { useAppSelector } from '@/lib/hooks';
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+    const session = useAppSelector((state) => state.session);
+
+    if (session.jwt.length == 0 || !session.jwt) {
+        redirect('/');
+    }
+
+    return { children };
+};
+
+export default ProtectedRoute;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,12 +5,12 @@ import PageWithNavbar from '../components/PageWithNavbar';
 import ProductCard from '../components/ProductCard';
 import { jwtDecode } from 'jwt-decode';
 import { useEffect, useState } from 'react';
-import { API_URL } from '../constants';
 import { setUser } from '@/lib/features/userSlice';
 import { User } from '@/lib/types/User';
 import { Item } from '@/lib/types/Item';
 import { getUser } from '@/lib/backend/users';
 import { getAllItems } from '@/lib/backend/items';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 const Home = () => {
     const userInState = useAppSelector((state) => state.user);
@@ -36,52 +36,59 @@ const Home = () => {
     }, []);
 
     return (
-        <PageWithNavbar>
-            <div className="w-full p-8 font-inter flex flex-col gap-y-8">
-                <div className="w-full flex justify-between items-center">
-                    <p className="text-red font-semibold text-3xl">
-                        Welcome back, {user?.user.name}!
-                    </p>
-                    <p className="text-blue font-semibold">
-                        You currently have{' '}
-                        <span className="font-black">{user?.user.credit}</span>{' '}
-                        {user?.user.credit == 1 ? 'credit' : 'credits'}.
-                    </p>
-                </div>
-                <div className="w-full bg-gradient-to-b from-dark-green to-green rounded-xl p-5 flex flex-col gap-y-4">
-                    <p className="text-white font-bold text-3xl">
-                        New Products
-                    </p>
-                    {/* <div className="flex gap-x-4">
+        <ProtectedRoute>
+            <PageWithNavbar>
+                <div className="w-full p-8 font-inter flex flex-col gap-y-8">
+                    <div className="w-full flex justify-between items-center">
+                        <p className="text-red font-semibold text-3xl">
+                            Welcome back, {user?.user.name}!
+                        </p>
+                        <p className="text-blue font-semibold">
+                            You currently have{' '}
+                            <span className="font-black">
+                                {user?.user.credit}
+                            </span>{' '}
+                            {user?.user.credit == 1 ? 'credit' : 'credits'}.
+                        </p>
+                    </div>
+                    <div className="w-full bg-gradient-to-b from-dark-green to-green rounded-xl p-5 flex flex-col gap-y-4">
+                        <p className="text-white font-bold text-3xl">
+                            New Products
+                        </p>
+                        {/* <div className="flex gap-x-4">
                         {dummyCategories.map((cat) => {
                             return <Category name={cat} key={cat} />;
                         })}
                     </div> */}
-                </div>
-                {/*Category*/}
-                <div className="flex flex-col gap-y-4">
-                    <p className="font-bold text-blue text-3xl">Shoes</p>
-                    <div className="w-full flex justify-between gap-x-4">
-                        {items.slice(0, 6).map((prod: Item) => (
-                            <ProductCard key={prod.id} item={prod} />
-                        ))}
                     </div>
-                </div>
-
-                <div className="flex flex-col gap-y-4">
-                    <p className="font-bold text-blue text-3xl">
-                        {'<500'} credits
-                    </p>
-                    <div className="w-full flex justify-between gap-x-4">
-                        {items
-                            .filter((item) => Number(item.price) < 500)
-                            .map((prod) => (
-                                <ProductCard key={`${prod.id}2}`} item={prod} />
+                    {/*Category*/}
+                    <div className="flex flex-col gap-y-4">
+                        <p className="font-bold text-blue text-3xl">Shoes</p>
+                        <div className="w-full flex justify-between gap-x-4">
+                            {items.slice(0, 6).map((prod: Item) => (
+                                <ProductCard key={prod.id} item={prod} />
                             ))}
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-y-4">
+                        <p className="font-bold text-blue text-3xl">
+                            {'<500'} credits
+                        </p>
+                        <div className="w-full flex justify-between gap-x-4">
+                            {items
+                                .filter((item) => Number(item.price) < 500)
+                                .map((prod) => (
+                                    <ProductCard
+                                        key={`${prod.id}2}`}
+                                        item={prod}
+                                    />
+                                ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </PageWithNavbar>
+            </PageWithNavbar>
+        </ProtectedRoute>
     );
 };
 
