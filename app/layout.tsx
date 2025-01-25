7dc1a1eff8ce25c0ae6e7a430e47a7293199e054
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-01-26 00:26:59 +0800

    Setup react-query and change queries to use react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import StoreProvider from './StoreProvider';
+import Providers from './providers';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -27,7 +28,9 @@ export default function RootLayout({
         <html lang="en">
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <StoreProvider>{children}</StoreProvider>
+                <Providers>
+                    <StoreProvider>{children}</StoreProvider>
+                </Providers>
             </body>
         </html>
     );
